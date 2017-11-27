fa015044734e381174f287998aab3def7d8069fd
Hau Chen <cettoana@gmail.com>
2017-11-27 12:15:06 +0800

    chore(package): upgrade react, react-dom to 16

--- src/scripts/components/Line.js
+++ src/scripts/components/Line.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import { line, area } from 'd3-shape'
 
 class Line extends React.Component {
